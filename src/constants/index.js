2cef2399482c49308076656259c9980cc4465aa9
Akieboye Godgift <akiboyegodgift.com>
2025-05-27 00:59:32 +0100

    Build config

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,7 @@ import {
   jobit,
   tripguide,
   threejs,
+  agmtechpluse,
 } from "../assets";
 
 export const navLinks = [
@@ -116,21 +117,21 @@ const technologies = [
 
 const experiences = [
   {
-    title: "Web Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    title: "Web Developer Intern",
+    company_name: "AGM Teech Pluse",
+    icon: agmtechpluse,
     iconBg: "#383E56",
     date: "March 2020 - April 2021",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Learning from Senior Software Developer and emulate thier algorithms and problem solving skills",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
+    title: "FullStack Web Developer",
+    company_name: "Jobeny Tech",
     icon: tesla,
     iconBg: "#E6DEDD",
     date: "Jan 2021 - Feb 2022",
@@ -147,19 +148,6 @@ const experiences = [
     icon: shopify,
     iconBg: "#383E56",
     date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
